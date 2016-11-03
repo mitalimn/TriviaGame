96f447f29193fd3d017e1074180b97ee4d60521a
mitalimn <mitalinaik@yahoo.com>
2016-11-03 10:59:38 -0500

    changed the layout script

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,255 +1,244 @@
+var app = {
+
+
+	qAndA:[{question: "Who is the librarian at Hogwarts?",
+			pos1: "Madam Rosmerta",
+			pos2: "Madam Pince",
+			pos3: "Madam Skeeter",
+			pos4: "Madam Hootch",
+			imgUrl: "./assets/images/Madam_Pince.png"},
+		   {question: "Where does Charlie Weasley work with dragons??",
+			pos1: "Ireland",
+			pos2: "Transylvania",
+			pos3: "Romania",
+			pos4: "Estonia",
+			imgUrl: "./assets/images/Romania.jpg"},
+		   {question: "What was Tom Riddle\'s mother\'s maiden name?",
+			pos1: "Riddle",
+			pos2: "Clearwater",
+			pos3: "Peverell",
+			pos4: "Gaunt",
+			imgUrl: "./assets/images/Gaunt.png"},
+		   {question: "What is the symbol for Hufflepuff house?",
+			pos1: "Badger",
+			pos2: "Eagle",
+			pos3: "Snake",
+			pos4: "Lion",
+			imgUrl: "./assets/images/Badger.png"},
+		   {question: "When is Harry Potter\'s Birthday?",
+		    pos1: "30th August",
+		    pos2: "30th July",
+		    pos3: "31st July",
+		    pos4: "30th June",
+			imgUrl: "./assets/images/Birthday.jpg"}],
+
+	correctAnswers: ['Madam Pince', 'Romania', 'Gaunt', 'Badger', '31st July'],
+	userAnswers: [],
+
+	incrementQs: 0,
+	beginInt: 0,
+
+	timer: 30,
+	btnClicked: false,
+	numberCorrect: 0,
+	numberIncorrect: 0,
+	numberUnAnswered: 0,
+	playMusic: new Audio("./assets/sounds/quizsound.mp3"),
+
+	beginGame: function(){
+		app.playMusic.play();	
+		if(app.incrementQs == app.qAndA.length){
+
+			app.gameFinished();
+			app.timer = 30;
+
+		} else {
+
+			if(app.incrementQs >= 1){
+				clearInterval(app.displayNextInt);
+				$('#gameStart').show();
+				$('#divAnswers').hide();
+				app.timer = 30;
+				$('#time').html(app.timer); //??
+			}
+
+			$('p.questions').html(app.qAndA[app.incrementQs].question);
+			$('p.answer1').html(app.qAndA[app.incrementQs].pos1);
+			$('p.answer2').html(app.qAndA[app.incrementQs].pos2);
+			$('p.answer3').html(app.qAndA[app.incrementQs].pos3);
+			$('p.answer4').html(app.qAndA[app.incrementQs].pos4);
+
+			app.beginInt = setInterval(app.count, 1000);
+
+		}
+
+
+	},
+
+	count: function(){
+
+		app.timer--;
+		$('#time').html(app.timer);
+
+		if(app.timer == 0){
+
+			app.oufOfTime();
+			app.playMusic.pause();
+
+		} else if(app.btnClicked == true && app.correctAnswers[app.incrementQs] == app.userAnswers[app.incrementQs]){
+		
+			app.answersCorrect();
+			app.playMusic.play();
+
+		} else if(app.btnClicked == true && app.correctAnswers[app.incrementQs] != app.userAnswers[app.incrementQs]){
+
+			app.answersWrong();
+			app.playMusic.pause();
+		}
+
+	},
+
+	answersCorrect: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#outOfTime').hide();
+		$('#wrongMsg').hide();	
+		$('#correctMsg').show();
+		$('#pCorrectAnswer').hide();	
+		$('#answers').css('display', 'block');
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(app.timer);
+
+		clearInterval(app.beginInt);
+
+		var newImg = $("<img>").attr('src', app.qAndA[app.incrementQs].imgUrl).attr('width', '130px','height', '130px').attr('id', 'correctMovieImage');
+
+		$('#pic').append(newImg);		
+		app.btnClicked = false;
+
+		app.displayNextInt = setInterval(app.beginGame, 3000);
+		app.numberCorrect++;
+		app.incrementQs++;
+	},
+
+	answersWrong: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#outOfTime').hide();
+		$('#wrongMsg').show();
+		$('#correctMsg').hide();
+		$('#pCorrectAnswer').show();
+		$('#pCorrectAnswer span').html(app.correctAnswers[app.incrementQs]);
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(app.timer);
+		clearInterval(app.beginInt);
+
+		var newImg = $("<img>").attr('src', app.qAndA[app.incrementQs].imgUrl).attr('width', '115px').attr('id', 'correctMovieImage');
+
+		$('#pic').append(newImg);
+
+		app.btnClicked = false;
+		app.displayNextInt = setInterval(app.beginGame, 5000);
+		app.numberIncorrect++;
+		app.incrementQs++;
+	},
+
+	oufOfTime: function(){
+
+		if(newImg != ""){
+
+			$('#pic').empty();
+		}
+
+		app.userAnswers.push(""); // placeholder, MAY NEED TO CHANGE
+		$('#divAnswers').show();
+		$('#gameStart').hide();
+		$('#pCorrectAnswer span').html(app.correctAnswers[app.incrementQs]);
+		$('#pCorrectAnswer').show();
+		$('#correctMsg').hide();
+		$('#wrongMsg').hide();		
+		$('#timeRemaining').css('display', 'block');
+		$('#elapsedTime').html(app.timer);	
+		clearInterval(app.beginInt);
+		var newImg = $("<img>").attr('src', app.qAndA[app.incrementQs].imgUrl).attr('width', '115px').attr('id', 'correctMovieImage');
+
+		$('#pic').append(newImg);
+
+		app.numberUnAnswered++;
+
+		app.displayNextInt = setInterval(app.beginGame, 5000);
+
+		app.incrementQs++;	
+
+	},
+
+	restart: function(){
+
+		app.incrementQs = 0;
+		app.userAnswers.length = 0;
+		$('#time').html("30");
+
+		app.beginGame();
+		$('#gameStart').show();
+		$('#gameComplete').hide();
+		$('#restartPlaceholder').css('display', 'none');
+		clearInterval(app.displayNextInt);
+		$('#elapsedTime').empty();
+		app.numberCorrect = 0;
+		app.numberIncorrect = 0;
+		app.numberUnAnswered = 0;
+	},
+
+	gameFinished: function(){
+
+		$('#restartPlaceholder').css('display', 'block');
+		$('#divAnswers').hide();
+		$('#gameStart').hide();
+
+		$('#gameComplete').css('display', 'block');
+
+		$('#gameOverCorrect span').html(app.numberCorrect);
+		$('#gameOverIncorrect span').html(app.numberIncorrect);
+		$('#unanswered span').html(app.numberUnAnswered);
+		app.timer = 30;
+	}
+};
+
+
 $(document).ready(function(){
-var number=30;
-	var triviaGame = {
-		firstPage :$('#firstPage'),
-		quizPage: $('#quizPage'),
-		gameEnd:$('#gameEnd'),
-		winCount :0,
-		totalAnswers: 5,
-		rightAnswerCount:0,
-		wrongAnswerCount:0,
-		notAttemptedCount:0,
-		totalCorrect: $('#totalCorrect'),
-		totalIncorrect:$('#totalIncorrect'),
-		totalNotAttempted:$('#totalNotAttempted'),
-		displayRightWrong:$('#displayRightWrong'),
-		answersUL:$('#answersUL'),
-		timeSeconds:$('#timeSeconds'),
-		playButton: $('#playButton'),
-		optionsLi:[],
-		answersUL:$('#answersUL'),
-		qdisplay:$('.qdisplay'),
-		questionCount:4,
+
+	$('#begin').on('click', function(){
+
+		$('div#gameStart').css('display', 'block');
+		$('#btnWrapper').css('display', 'none');
+		$('.questions').html(app.beginGame);
+
+	});
+
+	$('.answers').on('click', function(){
+
+		app.userAnswers.push($(this).text());
+		app.btnClicked = true;
+
+	});
+
+	$('#restartPlaceholder').on('click', function(){
+
+		app.restart();
 		
-		//Arrays for storing Question and Answers
-
-		questionAnswers : [{
-			question : 'Who is the librarian at Hogwarts?',
-			answers : [{
-				answer : 'Madam Rosmerta',
-				correct: false,
-			},
-			{
-				answer :'Madam Pince',
-				correct :true,
-			},
-			{
-				answer: 'Madam Skeeter',
-				correct : false,
-			},
-			{
-				answer :'Madam Hootch',
-				correct : false,
-			}]
-			
-		},{
-			question : 'Where does Charlie Weasley work with dragons?',
-			answers :[{
-				answer : 'Ireland',
-				correct : false,
-			},
-			{
-				answer : 'Romania',
-				correct : true,
-			} ,
-			{
-				answer : 'Transylvania',
-				correct : false,
-			},
-			{
-				answer : 'Estonia',
-				correct : false,
-			}]
-		},
-		{
-			question : 'What was Tom Riddle\'s mother\'s maiden name?',
-			answers: [{
-				answer :'Riddle',
-				correct :false,
-			},
-
-			{
-				answer : 'Clearwater',
-				correct : false,
-			},
-			{
-				answer : 'Peverell',
-				correct : false ,
-			},
-			{
-				answer : 'Gaunt',
-				correct : true,
-			}]
-		},
-		{
-			question : 'What is the symbol for Hufflepuff house?',
-			answers :[{
-				answer : 'Badger',
-				correct : true,
-			},
-			{
-				answer : 'Lion',
-				correct : false,
-			},
-			{
-				answer : 'Eagle',
-				correct : false,
-			},{
-				answer : 'Snake',
-				correct : false,
-			}]
-		},
-		{
-			question : 'When is Harry Potter\'s Birthday?',
-			answers: [{
-				answer : '30th July',
-				correct : false,
-			},
-			{
-				answer : '31st July',
-				correct : true,
-			},
-			{
-				answer : '31st August',
-				correct : false,
-			},{
-				answer : '30th June',
-				correct :  false,
-			}],
-		}],//Ends Question Ans array
-
-		//Function to display 4 options to the question displayed
-		answerCount:4,
-		current_question: 0,
-		rightAnswer: null,
-
-		hideGameScreen : function(){
-			console.log("Inside hide game screenfunction");
-			this.quizPage.hide();
-		},
-		hideGameEnd : function(){
-			this.gameEnd.hide();
-		},
-
-		showGameScreen : function(){
-			this.quizPage.show();
-		},
-		showGameEndScreen : function(){
-			this.gameEnd.show();
-		},
-
-		displayOption : function(){
-			console.log("display options");
-			for (i=0;i<this.answerCount; i++){
-				newLi = document.createElement('li');
-				$(newLi).addClass('list-answers options-Li').attr('data-index',i);
-				this.optionsLi.push(newLi); //optionsLi is an array
-				$(this.answersUL).append(newLi);
-			}
-		},
-
-		displayQsAs : function(){
-			console.log("display qstn");
-			this.qdisplay.html(this.questionAnswers[this.current_question].question);
-
-				//display options for the above question
-
-			for(var j=0;j<4;j++){
-				console.log("all 4 option");
-				console.log(this.questionAnswers[this.current_question].answers[j].answer);	
-				console.log(this.optionsLi);
-				$(this.optionsLi[j]).html(this.questionAnswers[this.current_question].answers[j].answer);
-			}
-			
-		},
-
-		playGame : function(){
-			this.quizTimer();
-			console.log("playgame runs");
-		},
-
-		quizTimer : function(){
-
-			console.log("inside quizTimer function");
-            if(number!=0)
-			Window.counter = setInterval(function(){ // Decrease number by one.
-
-
-            console.log("inside decrement function" );
-            number--;
-            // Show the number in the #show-number tag.
-            $('#timeSeconds').html(number);
-
-            // Once number hits zero...
-            if (number< 0){
-                // ...run the stop function.
-                console.log("inside timer if");
-                //stop();
-                clearInterval(Window.counter);
-                $('#timeSeconds').html('0');
-                        
-         	} 1000});
-			triviaGame.showGameScreen();
-			triviaGame.showGameEndScreen();
-			triviaGame.playButton.fadeOut();
-		},
-
-		//restart function
-		restart : function(){
-
-		},
-
-		
-		//Based on the users option this funtion checks if its right or wrong 
-		guessTheAnswer : function(dataid){
-			clearInterval(Window.counter);
-
-			dataIndex = parseInt(dataid);
-			if(dataIndex === this.rightAnswer){
-				this.rightAnswerCount++;
-				this.displayRightWrong.html("Right Answer");
-			}
-			// else{
-			// 	this.wrongAnswerCount++;
-			// 	this.displayRightWrong.html("Wrong Answer - the correct answer is ");
-			// 	this.rightAnswer.optionsLi
-			// }
-		},
-
-		//function for timer
-   
-    
-
-	} //End TriviaGame
-	console.log("hiding game screen");
-	triviaGame.hideGameScreen();
-	triviaGame.hideGameEnd();
-	//display timer
-	console.log("timer");
-	$('#playButton').on('click', function(){
-		triviaGame.playGame();
 	});
-	//display questions
-
-
-	$('#restartButton').on('click', function(){
-
-	});
-
-	
-
-	triviaGame.displayOption();
-	triviaGame.displayQsAs();
-	//display options
-
-	//once the user click on the options 
-	//pass the data index of li element
-	
-	triviaGame.answersUL.on("click", 'options-Li', function(){
-		console.log("li clicked");
-		triviaGame.guessTheAnswer(this.getAttribute("data-index"));
-	});
-	
 
 });
 
